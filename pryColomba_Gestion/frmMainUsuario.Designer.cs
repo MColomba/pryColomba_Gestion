c9454b8a44a224f29f28ada0f92dd67ba751ddd4
MColomba <matiascolomba@gmail.com>
2025-05-16 20:39:17 -0300

    se agrega funcionamiento a la grilla

--- pryColomba_Gestion/frmMainUsuario.Designer.cs
+++ pryColomba_Gestion/frmMainUsuario.Designer.cs
@@ -31,26 +31,25 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabRegistro = new System.Windows.Forms.TabPage();
             this.tabReporte = new System.Windows.Forms.TabPage();
+            this.btnListarProveedores = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.lblUsuario = new System.Windows.Forms.ToolStripStatusLabel();
-            this.btnListarProveedores = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.lblFecha = new System.Windows.Forms.ToolStripStatusLabel();
+            this.dgvProveedores = new System.Windows.Forms.DataGridView();
             this.tabControl1.SuspendLayout();
             this.tabReporte.SuspendLayout();
             this.statusStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProveedores)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabRegistro);
             this.tabControl1.Controls.Add(this.tabReporte);
-            this.tabControl1.Location = new System.Drawing.Point(40, 26);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(216, 81);
+            this.tabControl1.Size = new System.Drawing.Size(141, 81);
             this.tabControl1.TabIndex = 0;
             // 
             // tabRegistro
@@ -58,22 +57,31 @@
             this.tabRegistro.Location = new System.Drawing.Point(4, 22);
             this.tabRegistro.Name = "tabRegistro";
             this.tabRegistro.Padding = new System.Windows.Forms.Padding(3);
-            this.tabRegistro.Size = new System.Drawing.Size(239, 55);
+            this.tabRegistro.Size = new System.Drawing.Size(208, 55);
             this.tabRegistro.TabIndex = 0;
             this.tabRegistro.Text = "Registro";
             this.tabRegistro.UseVisualStyleBackColor = true;
             // 
             // tabReporte
             // 
-            this.tabReporte.Controls.Add(this.button2);
             this.tabReporte.Controls.Add(this.btnListarProveedores);
             this.tabReporte.Location = new System.Drawing.Point(4, 22);
             this.tabReporte.Name = "tabReporte";
             this.tabReporte.Padding = new System.Windows.Forms.Padding(3);
-            this.tabReporte.Size = new System.Drawing.Size(208, 55);
+            this.tabReporte.Size = new System.Drawing.Size(133, 55);
             this.tabReporte.TabIndex = 1;
             this.tabReporte.Text = "Reportes";
             this.tabReporte.UseVisualStyleBackColor = true;
+            // 
+            // btnListarProveedores
+            // 
+            this.btnListarProveedores.Location = new System.Drawing.Point(7, 16);
+            this.btnListarProveedores.Name = "btnListarProveedores";
+            this.btnListarProveedores.Size = new System.Drawing.Size(114, 23);
+            this.btnListarProveedores.TabIndex = 0;
+            this.btnListarProveedores.Text = "Listado Proveedores";
+            this.btnListarProveedores.UseVisualStyleBackColor = true;
+            this.btnListarProveedores.Click += new System.EventHandler(this.btnListarProveedores_Click);
             // 
             // statusStrip1
             // 
@@ -92,44 +100,30 @@
             this.lblUsuario.Size = new System.Drawing.Size(118, 17);
             this.lblUsuario.Text = "toolStripStatusLabel1";
             // 
-            // btnListarProveedores
-            // 
-            this.btnListarProveedores.Location = new System.Drawing.Point(7, 16);
-            this.btnListarProveedores.Name = "btnListarProveedores";
-            this.btnListarProveedores.Size = new System.Drawing.Size(114, 23);
-            this.btnListarProveedores.TabIndex = 0;
-            this.btnListarProveedores.Text = "Listado Proveedores";
-            this.btnListarProveedores.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(127, 16);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(40, 128);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 2;
-            // 
             // lblFecha
             // 
             this.lblFecha.Name = "lblFecha";
             this.lblFecha.Size = new System.Drawing.Size(118, 17);
             this.lblFecha.Text = "toolStripStatusLabel1";
             // 
+            // dgvProveedores
+            // 
+            this.dgvProveedores.AllowUserToAddRows = false;
+            this.dgvProveedores.AllowUserToDeleteRows = false;
+            this.dgvProveedores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProveedores.Location = new System.Drawing.Point(12, 128);
+            this.dgvProveedores.Name = "dgvProveedores";
+            this.dgvProveedores.ReadOnly = true;
+            this.dgvProveedores.Size = new System.Drawing.Size(776, 297);
+            this.dgvProveedores.TabIndex = 2;
+            this.dgvProveedores.Visible = false;
+            // 
             // frmMainUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvProveedores);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tabControl1);
             this.Name = "frmMainUsuario";
@@ -138,7 +132,7 @@
             this.tabReporte.ResumeLayout(false);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProveedores)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,9 +145,8 @@
         private System.Windows.Forms.TabPage tabReporte;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel lblUsuario;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button btnListarProveedores;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvProveedores;
         private System.Windows.Forms.ToolStripStatusLabel lblFecha;
     }
 }
--- pryColomba_Gestion/frmMainUsuario.cs
+++ pryColomba_Gestion/frmMainUsuario.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Forms;
 
 namespace pryColomba_Gestion
@@ -17,6 +19,39 @@ namespace pryColomba_Gestion
             InitializeComponent();
             lblUsuario.Text = Usuario;
             lblFecha.Text = DateTime.Now.ToString();
+        }
+
+        private void btnListarProveedores_Click(object sender, EventArgs e)
+        {
+            dgvProveedores.Rows.Clear();
+            StreamReader lector = new StreamReader(@"../../Archivos/datosProveedor.txt");
+
+            int i = 0;
+            string registro = lector.ReadLine();
+            
+            if (registro != null)
+            {
+                string[] columnas = registro.Split('\t');
+                while (i < columnas.Length)
+                {
+                    DataGridViewColumn column = new DataGridViewColumn();
+                    column.Name = columnas[i];
+                    column.HeaderText = columnas[i];
+                    dgvProveedores.Columns.Add(column);
+                    i += 1;
+                }
+
+                registro = lector.ReadLine();
+                while (registro != null)
+                {
+                    string[] campos = registro.Split('\t');
+                    dgvProveedores.Rows.Add(campos[0], campos[1], campos[2], campos[3],
+                        campos[4], campos[5], campos[6], campos[7]);
+                    registro = lector.ReadLine();
+                }
+                lector.Close();
+                dgvProveedores.Visible = true;
+            }    
         }
     }
 }
